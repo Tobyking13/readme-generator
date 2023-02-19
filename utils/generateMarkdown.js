eb320d99b08b7e02cae4d90966785fce0b0959dc
Toby King <tobyking13@gmail.com>
2023-02-19 23:21:05 +0000

    adds tech badges at the top of readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,11 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   let badge;
-  let userContributionArr = data.contributing.split(',');
+  let userContributionArr = data.contributing.split(",");
   let userArr = [];
   let i = 0;
   let screenshot;
   let techArr = [];
-
 
   if (data.tech.includes("HTML") === true) {
     techArr.push(
@@ -39,39 +38,38 @@ function generateMarkdown(data) {
     );
   }
 
-  techArr = techArr.toString().replaceAll(',', '');
+  techArr = techArr.toString().replaceAll(",", "");
 
-
-  switch(data.licence) {
-    case 'Apache License 2.0':
+  switch (data.licence) {
+    case "Apache License 2.0":
       badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
       break;
-    case 'GNU General Public Licence v3.0':
+    case "GNU General Public Licence v3.0":
       badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
       break;
-    case 'MIT':
+    case "MIT":
       badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
       break;
-    case 'BSD 2-Clause \"Simplified\" Licence':
+    case 'BSD 2-Clause "Simplified" Licence':
       badge = `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
       break;
-    case 'BSD 3-Clause \"New\" or \"Revised\" Licence':
+    case 'BSD 3-Clause "New" or "Revised" Licence':
       badge = `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
       break;
-    case 'Boost Software License 1.0':
+    case "Boost Software License 1.0":
       badge = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
       break;
-    case 'Creative Commons Zero v1.0 Universal':
+    case "Creative Commons Zero v1.0 Universal":
       badge = `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`;
       break;
-    case 'Eclipse Public License 2.0':
+    case "Eclipse Public License 2.0":
       badge = `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
       break;
-    case 'Mozilla Public License 2.0':
+    case "Mozilla Public License 2.0":
       badge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
       break;
-    case 'NONE':
-      badge = `No badge available`
+    case "NONE":
+      badge = `No badge available`;
       break;
   }
 
@@ -79,22 +77,25 @@ function generateMarkdown(data) {
     userArr = [`N/A`];
   } else {
     while (i < userContributionArr.length) {
-      userArr.push(` Contribution ${i  + 1}.) https://github.com/${userContributionArr[i].trim()} <br> `);
+      userArr.push(
+        ` Contribution ${i + 1}.) https://github.com/${userContributionArr[
+          i
+        ].trim()} <br> `
+      );
       i++;
     }
-  userArr = JSON.stringify(userArr);
-    userArr = userArr.replaceAll('","', ' ');
-    userArr = userArr.replaceAll('["', ' ');
-    userArr = userArr.replaceAll('"]', ' ');
+    userArr = JSON.stringify(userArr);
+    userArr = userArr.replaceAll('","', " ");
+    userArr = userArr.replaceAll('["', " ");
+    userArr = userArr.replaceAll('"]', " ");
     userArr = userArr.trim();
   }
 
-  if(data.screenshot === 'none') {
-    screenshot = "none"
+  if (data.screenshot === "none") {
+    screenshot = "none";
   } else {
-    screenshot = `![${data.title}](${data.screenshot} \"${data.title}\")`
+    screenshot = `![${data.title}](${data.screenshot} \"${data.title}\")`;
   }
-
 
   // sections entitled => Description, Table of Contents (bulletpoints), Installation, Usage, Licence, Contributing, Tests, Questions
 
